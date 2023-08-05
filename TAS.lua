d2c551469b840b338e3fdf6c788f871e34dc5ac5
xSnqwyy <140879327+xSnqwyy@users.noreply.github.com>
2023-08-05 16:30:21 +0300

    Update TAS.lua

--- TAS.lua
+++ TAS.lua
@@ -9,39 +9,54 @@
 
 -- Instances
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
+local TASGui = Instance.new("ScreenGui")
+local TASFrame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local TAS = Instance.new("TextButton")
+local TasButton = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
 
 -- Properties
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+TASGui.Name = "TASGui"
+TASGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+TASGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.new(0.137255, 0.137255, 0.137255)
-Frame.BorderColor3 = Color3.new(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0, 0, 0.698795199, 0)
-Frame.Size = UDim2.new(0, 12, 0, 61)
+TASFrame.Name = "TASFrame"
+TASFrame.Parent = TASGui
+TASFrame.BackgroundColor3 = Color3.new(0.133333, 0.133333, 0.133333)
+TASFrame.BorderColor3 = Color3.new(0, 0, 0)
+TASFrame.BorderSizePixel = 0
+TASFrame.Position = UDim2.new(0, 0, 0.694779098, 0)
+TASFrame.Size = UDim2.new(0, 14, 0, 64)
 
-UICorner.Parent = Frame
+UICorner.Parent = TASFrame
 
-TAS.Name = "TAS"
-TAS.Parent = Frame
-TAS.BackgroundColor3 = Color3.new(1, 1, 1)
-TAS.BorderColor3 = Color3.new(0, 0, 0)
-TAS.BorderSizePixel = 0
-TAS.Position = UDim2.new(0.15034993, 0, 0.0543132834, 0)
-TAS.Size = UDim2.new(0, 126, 0, 50)
-TAS.Font = Enum.Font.SourceSans
-TAS.Text = "TAS"
-TAS.TextColor3 = Color3.new(0, 0, 0)
-TAS.TextSize = 14
+TasButton.Name = "TasButton"
+TasButton.Parent = TASGui
+TasButton.BackgroundColor3 = Color3.new(1, 1, 1)
+TasButton.BorderColor3 = Color3.new(0, 0, 0)
+TasButton.BorderSizePixel = 0
+TasButton.Position = UDim2.new(0.00878293626, 0, 0.714859426, 0)
+TasButton.Size = UDim2.new(0, 107, 0, 45)
+TasButton.Font = Enum.Font.LuckiestGuy
+TasButton.Text = "TAS"
+TasButton.TextColor3 = Color3.new(0, 0, 0)
+TasButton.TextSize = 20
 
-UICorner_2.Parent = TAS
-TAS.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/xSnqwyy/TAS/main/TAS.lua"))()
-end)
+UICorner_2.Parent = TasButton
+
+-- Scripts
+
+local function FHWXP_fake_script() -- TasButton.LocalScript 
+	local script = Instance.new('LocalScript', TasButton)
+
+
+end
+coroutine.wrap(FHWXP_fake_script)()
+local function HXAC_fake_script() -- TasButton.Script 
+	local script = Instance.new('LocalScript', TasButton)
+	TasButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xSnqwyy/TAS/main/Yuvnas%20Tas.lua"))()
+	end)
+end
+coroutine.wrap(HXAC_fake_script)()
