15999ad5166b0041599aed02c1e1360231dc8dab
xSnqwyy <140879327+xSnqwyy@users.noreply.github.com>
2023-08-05 16:14:03 +0300

    Update Yuvnas Tas.lua

--- Yuvnas Tas.lua
+++ Yuvnas Tas.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://www.roblox.com/asset/?id=10844579779"))()
+local Workspace
+local Workspace = script.Parent
+
+local function HXAC_fake_script()
+	local script = Instance.new('Script')
+	require(10844579779)
+end
