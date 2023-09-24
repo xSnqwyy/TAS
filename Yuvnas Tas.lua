37814474607cb3d5e837fb581219a278202ddf4c
xSnqwyy <140879327+xSnqwyy@users.noreply.github.com>
2023-09-24 13:47:04 +0300

    Update Yuvnas Tas.lua

--- Yuvnas Tas.lua
+++ Yuvnas Tas.lua
@@ -1,6 +1,6 @@
 local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
 for i, v in pairs(game:GetService("Workspace").MilBase.CoinContainer["Coin_Server"]:GetChildren())
-if v:IsA("Part")
+if v:IsA("Part") then
 rootPart.CFrame = v.CFrame wait(0.2)
 end
 end
