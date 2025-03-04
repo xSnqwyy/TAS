974ba21693f22ce5824ed5f7386cc8cd71c4d2ee
xSnqwyy <140879327+xSnqwyy@users.noreply.github.com>
2025-03-04 11:49:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-import os
 
-attack1 = "1"
-attack2 = "2"
-attack3 = "3"
-attack4 = "4"
-
-attackslist = ["[1] DDos Attack","[2] Brute Force Attack","[3] Phishing Attack","[4] Hydra Attack"]
-
-for attacklist in attackslist:
-  print(attacklist)
-  
-attacks = input("Choose an attack tool: ")
-
-if attacks == "1":
-  print("DDos attack loading...")
-elif attacks == "1":
-    print("DDos tool cannot loading because no files please install...")
-    
