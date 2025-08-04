e5050c20139c56e729bf6215e3e843bc385d52a0
xSnqwyy <140879327+xSnqwyy@users.noreply.github.com>
2025-08-04 23:11:39 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,18 @@
+local whitelist = {
+  "kguinh",
+  "Tembelhayvancook",
+  "Yuvnas"
+}
 
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+local hum = char:FindFirstChild("Humanoid")
+
+
+for _, player in ipairs(whitelist) do
+  if plr.Name == player then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xSnqwyy/TAS/refs/heads/main/R6Animation.lua"))()
+  else
+    error("you're not in white list")
+  end
+end
